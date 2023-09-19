eee44ca74ebce8b5f12c23ee30b0134739b6d5c3
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-09-19 10:11:31 +0200

    Practicing post request

--- src/app/people.service.ts
+++ src/app/people.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Observable } from "rxjs";
 
 @Injectable()
@@ -9,8 +9,36 @@ export class PeopleService{
     constructor(private http: HttpClient){}
 
 
+    // Including filter #1
+    // fetchPeople(): Observable<Object> {
+    //     return this.http.get('/assets/data/people.json?id=123');
+    // }
+
+
+    // Using options object
+    // fetchPeople(): Observable<Object> {
+    //     return this.http.get('/assets/data/people.json', {
+    //         params: new HttpParams().set('id', '123')
+    //     });
+    // }
+
+    // Sending several parameters with get request
+    // fetchPeople(): Observable<Object> {
+    //     const params = new HttpParams()
+    //     .set('id', '123')
+    //     .set('includeAddress', 'true');
+    //     return this.http.get('/assets/data/people.json', {
+    //         params: params
+    //     });
+    // }
+
+    // Sending the data using a post request
     fetchPeople(): Observable<Object> {
-        return this.http.get('/assets/data/people.json');
+        const newPerson = {
+            name: 'Pete'
+        };
+
+        return this.http.post('/assets/data/people.json', newPerson);
     }
 
 }
